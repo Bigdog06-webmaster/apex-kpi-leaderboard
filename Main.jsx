bd8be42ab333afb62f2445ebb50e0c32d9a1f550
Bigdog06-webmaster <matthew@apexhomesdxb.com>
2026-04-07 16:46:54 +0400

    Delete Main.jsx

--- Main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ApexApp from './ApexApp.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ApexApp />
-  </StrictMode>
-)
